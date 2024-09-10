09440adec4b27d1c0d39b131b196ee9a993ae006
Fahim-99 <fahimmuntasirruby@gmail.com>
2024-09-10 10:17:54 +0600

    update collection file

--- report.js
+++ report.js
@@ -3,6 +3,7 @@ require(' dotenv' ) . config();
 
 newman. run ({
     collection :`https://api.postman.com/collections/37996569-5f01c986-1329-4a7d-be2a-322da69cf0b4?access_key= ${process.env.secretKey}`,
+    collection: require ('./collection/collection.json'),
     reporters: 'htmlextra', 
     iterationCount : 1, 
     reporter: { 
